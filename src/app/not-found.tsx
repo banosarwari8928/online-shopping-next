54225777d9c02b943709c7c0d193457b5b42e8c5
banosarwari8928 <banosarwari8928@gmail.com>
2026-01-30 17:24:50 +0430

    worked n not-found and loading page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,13 +6,16 @@ import { Button } from '@/components/ui/button'
 
 function notfound() {
   return (
-    <div className='min-h-screen flex flex-col items-center justify-center '>
-        <Image  src={""} alt={`${APP_NAME} `}/>
+    <div className='min-h-screen flex flex-col items-center justify-center rounded-md '>
+        <Image  src={"/images/logo.svg"} alt={`${APP_NAME}`} width={50} height={50}/>
         <div className="p-6 text-center shadow-sm">
             <h1 className='text-3xl font-bold'>Not Found...</h1>
-            {/* <Button variant={'outline'} onClick={()=> (window.location}> */}
-                Back to Home
-            {/* </Button> */}
+            <p>Could not found the page you requested</p>
+            <Button 
+            variant={'outline'} 
+            onClick={()=> (window.location.href="/")}>
+                Back to Home 
+          </Button> 
         </div>
     </div>
   )
